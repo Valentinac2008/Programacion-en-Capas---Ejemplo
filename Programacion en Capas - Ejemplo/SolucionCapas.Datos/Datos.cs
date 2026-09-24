9677277758103e61e7b09dadd09ad3bb60d62c63
Valentinac2008 <valentinac613@alu.bue.edu.ar>
2026-09-24 20:18:16 -0300

    Update Datos.cs

--- Programacion en Capas - Ejemplo/SolucionCapas.Datos/Datos.cs
+++ Programacion en Capas - Ejemplo/SolucionCapas.Datos/Datos.cs
@@ -5,14 +5,13 @@ namespace SolucionCapas.Datos
 {
     public class PersonaDatos
     {
-        // Simulamos la BD con una lista de datos
+
         private List<(string Dni, string Nombre)> _tablaPersonas = new List<(string, string)>
         {
-            ("23269431", "Alberto"),
-            ("27576691", "Noemi")
+            ("98253332", "Pablo"),
+            ("78247691", "Carlos")
         };
 
-        // Devuelve una lista con los datos o null
         public (string Dni, string Nombre)? BuscarPorDni(string dni)
         {
             var resultado = _tablaPersonas.FirstOrDefault(p => p.Dni == dni);
